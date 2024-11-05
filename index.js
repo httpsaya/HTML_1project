1d37f0c5dd99251a6393eac248f0c99d1ccb1e5a
httpsaya <157175266+httpsaya@users.noreply.github.com>
2024-11-05 15:20:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typewriter-text", {
+    strings: [
+        " ",
+        "Student",
+        "Kazakh",
+        "Girl",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+
+});
